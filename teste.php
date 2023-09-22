558d0fa66adbab3271b7441d68037ef28882bee3
thbomfim <thiagobomfimofc@gmail.com>
2023-09-22 00:58:55 -0300

    arquivo de testes

--- teste.php
+++ teste.php
@@ -10,21 +10,28 @@ include("inc/config.inc.php");
 $pdoo = new PDO("mysql:host=localhost;dbname=submaster", "root", "thbomfim");
 
 
-$tinfo = $pdoo->query("SELECT name, text, authorid, crdate, views, fid, pollid from fun_topics WHERE id='1'")->fetch();   
-
-echo "<pre>nome: $tinfo[0]<br>
-      texto: $tinfo[1]<br>
-      views: $tinfo[2]<br></pre>";
-echo "$tnm";
-
+        $sql = $pdo->prepare("DELETE FROM sudo_topic WHERE id = '1' "); 
+        $sql->rowCount();
+            
+        if ($sql >= 0) {
+            echo "seila";
+        }else {
+            echo "ja nem sei mas..";
+        }
+        
+            /*
+            echo"<pre>";
+            echo "var_dump($sql)";
+            echo "</pre>";
+            */
 $fcats = $pdo->query("SELECT id, name FROM sudo_fcat ORDER BY position, id")->fetchAll();
 
 //$fcats = $pdo->query("SELECT id FROM sudo_subcat WHERE catid ='2'")->fetch();
 
-var_dump($fcats);
+//var_dump($fcats);
 
 echo "<br>";
 $forums = $pdoo->query("SELECT id, name FROM fun_forums WHERE cid='1' AND clubid='0' ORDER BY position, id, name")->fetch();
-var_dump($forums);
+//var_dump($forums);
 
 ?>
